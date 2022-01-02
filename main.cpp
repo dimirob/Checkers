04d7d3066bc01f2cae59bd9a46fa3fcc7f632212
dimirob <dimitriosrvosnakis@gmail.com>
2022-01-02 18:56:37 +0200

    Board insertion

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 void draw() {
 	graphics::Brush br;
 	br.outline_opacity = 0.0f;
-	br.texture = "D:\cpp\Checkers\bin\assets\board.png";
+	br.texture = ASSET_PATH+std::string("Board.png");
 	graphics::drawRect(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2, CANVAS_WIDTH, CANVAS_HEIGHT, br);
 }
 
